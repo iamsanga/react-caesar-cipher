b146d8b3dc6d5bebe694123b1bfbc812df9c9911
iamsanga <iamsanga7@gmail.com>
2020-04-13 00:13:58 +0530

    Update Shift.js
    component rebuild

--- src/components/Shift.js
+++ src/components/Shift.js
@@ -1,26 +1,46 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 class Shift extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shift: ""
+    };
+  }
 
+  handleChange = e => {
+
+    this.setState({ shift: e.target.value });
+
+    this.props.handleChange(e.target.value);
+
+  };
   render() {
     return (
       <div className="shift">
         <center>
           <Select
             autoWidth={true}
-            displayEmpty>
-            <MenuItem value="" disabled>Enter shift amount</MenuItem>
+            onChange={this.handleChange}
+            value={this.state.shift}
+            displayEmpty
+          >
+            <MenuItem value={this.state.shift} disabled>
+              Enter shift amount
+            </MenuItem>
+            {[0, 1, 2, 3, 4, 5].map(String).map(x => (
+              <MenuItem key={x} value={x}>
+                {x}
+              </MenuItem>
+            ))}
           </Select>
         </center>
       </div>
     );
   }
-  
-};
-
+}
 
 export default Shift;
